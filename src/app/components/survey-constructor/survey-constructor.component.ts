4e2d19210e4e5fe9ebd988649f3adeb2cdc5e97d
Kirill Shishov <skireal@mail.ru>
2023-08-08 18:46:24 +0100

    add: init survey-constructor component

---
+++ src/app/components/survey-constructor/survey-constructor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-survey-constructor',
+  templateUrl: './survey-constructor.component.html',
+  styleUrls: ['./survey-constructor.component.scss']
+})
+export class SurveyConstructorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
