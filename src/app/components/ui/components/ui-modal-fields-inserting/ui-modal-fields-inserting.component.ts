5ae0979fc6774aa020ca5b1e7c88e0df3af1020b
Kirill Shishov <skireal@mail.ru>
2023-08-18 18:21:34 +0100

    feat: add feild key generator

--- src/app/components/ui/components/ui-modal-fields-inserting/ui-modal-fields-inserting.component.ts
+++ src/app/components/ui/components/ui-modal-fields-inserting/ui-modal-fields-inserting.component.ts
@@ -2,7 +2,7 @@ import { Component, EventEmitter, OnInit } from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap/modal';
 
 interface FormField {
-  key: string;
+  key?: string;
   label: string;
   type: string;
   options?: { value: any; label: string }[];
@@ -17,12 +17,12 @@ export class UIModalFieldsInsertingComponent implements OnInit {
   onBlockSelect: EventEmitter<FormField> = new EventEmitter<FormField>();
 
   availableBlocks: FormField[] = [
-    { key: 'text343', label: 'Text Field', type: 'input' },
+    { key: '', label: 'Text Field', type: 'input' },
     { key: 'date', label: 'Date Field', type: 'date' },
     {
       key: 'dropdown',
       label: 'Dropdown',
-      type: 'dropdown',
+      type: 'select',
       options: [
         { value: 'option1', label: 'Option 1' },
         { value: 'option2', label: 'Option 2' }
@@ -38,8 +38,13 @@ export class UIModalFieldsInsertingComponent implements OnInit {
   selectBlock(blockType: string) {
     const selectedBlock = this.availableBlocks.find((block) => block.type === blockType);
     if (selectedBlock) {
+      selectedBlock.key = selectedBlock.key || this.generateUniqueId().toString();
       this.onBlockSelect.emit(selectedBlock);
       this.modalRef.hide();
     }
+  }
+
+  generateUniqueId(): number {
+    return Math.floor(Math.random() * 1000000);
   }
 }
