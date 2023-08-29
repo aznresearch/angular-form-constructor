1e8aa2ae63d4e8d14ac374733e58e57904038b4a
Kirill Shishov <skireal@mail.ru>
2023-08-29 14:21:35 +0100

    feat: add clear form logic

--- src/app/components/ui/ui.component.ts
+++ src/app/components/ui/ui.component.ts
@@ -4,6 +4,7 @@ import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 import { UIModalFieldsInsertingComponent } from './components/ui-modal-fields-inserting/ui-modal-fields-inserting.component';
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { UIModalFieldPropertiesComponent } from './components/ui-modal-field-properties/ui-modal-field-properties.component';
+import { SharedModalConfirmationComponent } from '../shared/shared-modal-confirmation/shared-modal-confirmation.component';
 
 interface FormField {
   name: string;
@@ -26,6 +27,13 @@ export class UIComponent implements OnInit {
   addedFields: FormField[] = [];
 
   currentStep = 0;
+
+  modalOptions = {
+    initialState: {
+      message: 'Are you sure you want to clear the form?'
+    },
+    class: 'modal-md'
+  };
 
   constructor(private modalService: BsModalService) {}
 
@@ -109,6 +117,26 @@ export class UIComponent implements OnInit {
       this.currentStep--;
       this.goToStep(this.currentStep);
     }
+  }
+
+  clearFormConfirmation() {
+    const modalRef = this.modalService.show(SharedModalConfirmationComponent, this.modalOptions);
+
+    modalRef.content.close.subscribe((result: boolean) => {
+      if (result) {
+        this.clearCurrentStep();
+      }
+    });
+  }
+
+  clearCurrentStep() {
+    this.addedFields = [];
+    this.formData[this.currentStep] = [];
+    Object.keys(this.dynamicForm.controls).forEach((controlName) => {
+      if (this.dynamicForm.contains(controlName)) {
+        this.dynamicForm.removeControl(controlName);
+      }
+    });
   }
 
   preventDefault(event: Event) {
